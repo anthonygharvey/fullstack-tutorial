b9fd9b6e9f4b9186748945279317472da40329e3
Anthony Harvey <anthony.harvey@fanconnect.tv>
2019-10-16 17:11:34 -0400

    Fetch data with queries

--- start/client/src/pages/launch.js
+++ start/client/src/pages/launch.js
@@ -1,5 +1,41 @@
-import React from 'react';
+import React from "react";
+import { useQuery } from "@apollo/react-hooks";
+import gql from "graphql-tag";
 
-export default function Launch() {
-  return <div />;
+import { Loading, Header, LaunchDetail } from "../components";
+import { ActionButton } from "../containers";
+
+import { LAUNCH_TILE_DATA } from "./launches";
+
+export const GET_LAUNCH_DETAILS = gql`
+  query LaunchDetails($launchId: ID!) {
+    launch(id: $launchId) {
+      site
+      isBooked
+      rocket {
+        type
+      }
+      ...LaunchTile
+    }
+  }
+  ${LAUNCH_TILE_DATA}
+`;
+
+// 'launchId' is passed as a prop from the router
+export default function Launch({ launchId }) {
+  const { data, loading, error } = useQuery(GET_LAUNCH_DETAILS, {
+    variables: { launchId }
+  });
+  if (loading) return <Loading />;
+  if (error) return <p>ERROR: {error.message}</p>;
+
+  return (
+    <>
+      <Header image={data.launch.mission.missionPatch}>
+        {data.launch.mission.name}
+      </Header>
+      <LaunchDetail {...data.launch} />
+      <ActionButton {...data.launch} />
+    </>
+  );
 }
