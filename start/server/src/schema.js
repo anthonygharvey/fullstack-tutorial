b73474c8abbca753e35c4d418221ebbd4ad4b94c
Anthony Harvey <anthony.harvey@fanconnect.tv>
2019-10-16 09:52:12 -0400

    Add resolvers

--- start/server/src/schema.js
+++ start/server/src/schema.js
@@ -3,10 +3,31 @@ const { gql } = require("apollo-server");
 const typeDefs = gql`
   # entry point into the schema that describes what data we can fetch
   type Query {
-    launches: [Launch]!
+    # launches: [Launch]!
+    launches( # replace the current launches query with this one.
+      """
+      The number of results to show. Must be >= 1. Default = 20
+      """
+      pageSize: Int
+      """
+      If you add a cursor here, it will only return results _after_ this cursor
+      """
+      after: String
+    ): LaunchConnection!
     launch(id: ID!): Launch
     # Queries for the current user
     me: User
+  }
+
+  """
+  Simple wrapper around our list of launches that contains a cursor to the
+  last item in the list. Pass this cursor to the launches query to fetch results
+  after these.
+  """
+  type LaunchConnection {
+    cursor: String!
+    hasMore: Boolean!
+    launches: [Launch]!
   }
 
   type Launch {
@@ -31,7 +52,7 @@ const typeDefs = gql`
 
   type Mission {
     name: String
-    missionPatch(size: PatchSize): String
+    missionPatch(mission: String, size: PatchSize): String
   }
 
   enum PatchSize {
